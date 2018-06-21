28a1f8f85e4bb4286a243f0079351b7112d7b468
aakbik <alan.akbik@zalando.de>
2018-06-21 16:45:10 +0200

    expanded documentation

--- predict.py
+++ predict.py
@@ -7,5 +7,5 @@ sentence: Sentence = Sentence('George Washington went to Washington .')
 tagger.predict(sentence, tag_type='ner')
 
 print('Analysing %s' % sentence)
-print('\n# The following NER tags are found: \n')
+print('\nThe following NER tags are found: \n')
 print(sentence.to_ner_string())
