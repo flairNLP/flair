75e61dc717c463aef60704dee26eaad8c10adb1d
aakbik <alan.akbik@zalando.de>
2018-06-22 11:24:40 +0200

    added pre-trained model for chunking

--- flair/tagging_model.py
+++ flair/tagging_model.py
@@ -103,7 +103,11 @@ class SequenceTaggerLSTM(nn.Module):
         model_file = None
         # news-english-forward
         if model.lower() == 'ner':
-            base_path = 'https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/models/ner-test.pt'
+            base_path = 'https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/models/ner-conll03.pt'
+            model_file = cached_path(base_path)
+
+        if model.lower() == 'chunk':
+            base_path = 'https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/models/chunk-conll2000.pt'
             model_file = cached_path(base_path)
 
         if model_file is not None:
