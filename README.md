8c5bcfcc934bd3e446f434cf9e4a7fcd5e9f3833
aakbik <alan.akbik@zalando.de>
2018-07-19 15:04:03 +0200

    made project installable through pip

--- README.md
+++ README.md
@@ -47,18 +47,10 @@ Alan Akbik, Duncan Blythe and Roland Vollgraf.
 The project is based on PyTorch 0.4+ and Python 3.6+, because methods signatures and type hints are beautiful.
 If you do not have Python 3.6, install it first. [Here is how for Ubuntu 16.04](http://ubuntuhandbook.org/index.php/2017/07/install-python-3-6-1-in-ubuntu-16-04-lts/). 
 
-You can create an appropriate environment using virtualenv:
+Then, in your favorite virtual environment, simply do:
 
 ```
-virtualenv --python=[path/to/python3.6] [path/to/this/virtualenv]
-
-source [path/to/this/virtualenv]/bin/activate
-```
-
-Within this environment, install the package from the repository:
-
-```
-pip install git+https://github.com/zalandoresearch/flair
+pip install flair
 ```
 
 ### Example Usage
--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-   requirements = f.read().split('\n')
-
 setup(
-   name='flair',
-   version='0.1',
-   description='A very simple framework for state-of-the-art NLP',
-   author='Alan Akbik',
-   author_email='alan.akbik@zalando.de',
-   url='https://github.com/zalandoresearch/flair',
-   packages=find_packages(exclude='test'),  #same as name
-   install_requires=requirements
+    name='flair',
+    version='0.1.1',
+    description='A very simple framework for state-of-the-art NLP',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Alan Akbik',
+    author_email='alan.akbik@zalando.de',
+    url='https://github.com/zalandoresearch/flair',
+    packages=find_packages(exclude='test'),  # same as name
+    license='MIT',
+    install_requires=[
+        'torch==0.4.0',
+        'awscli==1.14.32',
+        'gensim==3.4.0',
+        'typing==3.6.4',
+        'tqdm==4.23.4',
+        'segtok==1.5.6'
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
 )
