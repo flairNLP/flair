819304290de17fdd1eaf6652ca74c6026af50ce5
aakbik <alan.akbik@zalando.de>
2018-07-30 10:55:18 +0200

    GH-15: added test for training LMs | pytest requirement

---
+++ tests/test_language_model_trainer.py
@@ -0,0 +1,33 @@
+import shutil
+
+from flair.data import Dictionary, Sentence
+from flair.embeddings import CharLMEmbeddings
+from flair.models import LanguageModel
+from flair.trainers.language_model_trainer import LanguageModelTrainer, TextCorpus
+
+# get default dictionary
+dictionary: Dictionary = Dictionary.load('chars')
+
+# init forward LM with 128 hidden states and 1 layer
+language_model: LanguageModel = LanguageModel(dictionary, is_forward_lm=True, hidden_size=128, nlayers=1)
+
+# get the example corpus and process at character level in forward direction
+corpus: TextCorpus = TextCorpus('resources/corpora/lorem_ipsum',
+                                dictionary,
+                                language_model.is_forward_lm,
+                                character_level=True)
+
+# train the language model
+trainer: LanguageModelTrainer = LanguageModelTrainer(language_model, corpus)
+trainer.train('./results', sequence_length=10, mini_batch_size=10, max_epochs=5)
+
+# use the character LM as embeddings to embed the example sentence 'I love Berlin'
+char_lm_embeddings = CharLMEmbeddings('./results/best-lm.pt')
+sentence = Sentence('I love Berlin')
+char_lm_embeddings.embed(sentence)
+print(sentence[1].embedding.size())
+
+# clean up results directory
+shutil.rmtree('./results')
+
+
