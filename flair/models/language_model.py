3de0ed4cc8f40fe7cfe020976e90f4280557665f
tabergma <tabergma@gmail.com>
2018-11-08 13:34:59 +0100

    GH-167: Ad method to generate text.

--- flair/models/language_model.py
+++ flair/models/language_model.py
@@ -1,9 +1,13 @@
+import logging
 import torch.nn as nn
 import torch
 import math
 from torch.autograd import Variable
 from typing import Dict, List
 from flair.data import Dictionary
+
+
+log = logging.getLogger(__name__)
 
 
 class LanguageModel(nn.Module):
@@ -148,3 +152,29 @@ class LanguageModel(nn.Module):
             'dropout': self.dropout
         }
         torch.save(model_state, file, pickle_protocol=4)
+
+    def generate_text(self, number_of_characters=1000):
+        log.info('Generating text ...')
+
+        characters = []
+
+        idx2item = self.dictionary.idx2item
+
+        # initial hidden state
+        hidden = self.init_hidden(1)
+        input = torch.rand(1, 1).mul(len(idx2item)).long()
+
+        for i in range(number_of_characters):
+            prediction, rnn_output, hidden = self.forward(input, hidden)
+            word_weights = prediction.squeeze().data.div(1.0).exp().cpu()
+            word_idx = torch.multinomial(word_weights, 1)[0]
+            input.data.fill_(word_idx)
+            word = idx2item[word_idx].decode('UTF-8')
+            characters.append(word)
+
+            if i % 100 == 0:
+                log.info(f'{i}/{number_of_characters} chars')
+
+        # print generated text
+        log.info('Generated text:')
+        log.info(''.join(characters))
