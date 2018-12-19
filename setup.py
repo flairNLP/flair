264c63be929d6c4e67c3a1df73c3f3aac2066a53
aakbik <alan.akbik@zalando.de>
2018-12-19 18:21:45 +0100

    GH-316: bump flair version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flair',
-    version='0.3.2',
+    version='0.4.0',
     description='A very simple framework for state-of-the-art NLP',
     long_description=open("README.md", encoding='utf-8').read(),
     long_description_content_type="text/markdown",
