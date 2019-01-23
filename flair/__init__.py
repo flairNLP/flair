765e230af51a03853c88957b94a9bad8757650c3
tabergma <tabergma@gmail.com>
2019-01-23 17:05:10 +0100

    GH-415: Explicit enable existing loggers.

--- flair/__init__.py
+++ flair/__init__.py
@@ -10,6 +10,7 @@ import logging.config
 
 logging.config.dictConfig({
     'version': 1,
+    'disable_existing_loggers': False,
     'formatters': {
         'standard': {
             'format': '%(asctime)-15s %(message)s'
