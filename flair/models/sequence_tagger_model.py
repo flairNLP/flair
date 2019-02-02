abc05efac6c565a4426460f9964f0410b8486e16
aakbik <alan.akbik@zalando.de>
2019-02-02 14:57:15 +0100

    GH-407: fix training on cuda without CRF

--- flair/models/sequence_tagger_model.py
+++ flair/models/sequence_tagger_model.py
@@ -429,10 +429,11 @@ class SequenceTagger(flair.nn.Model):
             for sentence_feats, sentence_tags, sentence_length in zip(features, tags, lengths):
                 sentence_feats = sentence_feats[:sentence_length]
 
-                tag_tensor = torch.LongTensor(sentence_tags)
-                tag_tensor = tag_tensor.to(flair.device)
+                # print(sentence_tags)
+                # tag_tensor = torch.LongTensor(sentence_tags)
+                # tag_tensor = tag_tensor.to(flair.device)
 
-                score += torch.nn.functional.cross_entropy(sentence_feats, tag_tensor)
+                score += torch.nn.functional.cross_entropy(sentence_feats, sentence_tags)
 
             return score
 
