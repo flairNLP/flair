GH-538: fix CUDA and backpropagation

flair/embeddings.py
@@ -1818,16 +1818,16 @@ class DocumentLSTMEmbeddings(DocumentEmbeddings):
     def embedding_length(self) -> int:   
         return self.__embedding_length
 
-    def embed(self, sentences: Union[List[Sentence], Sentence]):
+    def embed(self, sentences: Union[List[Sentence], Sentence], return_sequences: bool = False):
         """Add embeddings to all sentences in the given list of sentences. If embeddings are already added, update
          only if embeddings are non-static."""
 
-        if type(sentences) is Sentence:   
+        if type(sentences) is Sentence:
             sentences = [sentences]
 
         self.rnn.zero_grad()
 
-        sentences.sort(key=lambda x: len(x), reverse=True) 
+        sentences.sort(key=lambda x: len(x), reverse=True)
 
         self.embeddings.embed(sentences)
 
@@ -1892,16 +1892,23 @@ class DocumentLSTMEmbeddings(DocumentEmbeddings):
         # --------------------------------------------------------------------
         # EXTRACT EMBEDDINGS FROM LSTM
         # --------------------------------------------------------------------
-        for sentence_no, length in enumerate(lengths):
-            last_rep = outputs[length - 1, sentence_no]
 
-            embedding = last_rep
-            if self.bidirectional:
-                first_rep = outputs[0, sentence_no]
-                embedding = torch.cat([first_rep, last_rep], 0)
+        if return_sequences:
 
-            sentence = sentences[sentence_no]
-            sentence.set_embedding(self.name, embedding)
+            return outputs
+
+        else:
+
+            for sentence_no, length in enumerate(lengths):
+                last_rep = outputs[length - 1, sentence_no]
+
+                embedding = last_rep
+                if self.bidirectional:
+                    first_rep = outputs[0, sentence_no]
+                    embedding = torch.cat([first_rep, last_rep], 0)
+
+                sentence = sentences[sentence_no]
+                sentence.set_embedding(self.name, embedding)
 
     def _add_embeddings_internal(self, sentences: List[Sentence]):
         pass

flair/models/text_classification_model.py
@@ -48,12 +48,12 @@ class Attention(flair.nn.Model):
 
         if weighted_sum_candidates is None:
             if self.batch_first:
-                weighting = torch.sum(weights * attention_candidates, dim=1, keepdim=True)
+                weighting = torch.sum(weights * attention_candidates, dim=1, keepdim=True).to(flair.device)
             else:
-                weighting = torch.sum(weights * dummy, dim=1, keepdim=True)
+                weighting = torch.sum(weights * dummy, dim=1, keepdim=True).to(flair.device)
 
         else:
-            weighting = torch.sum(weights * weighted_sum_candidates, dim=1, keepdim=True)
+            weighting = torch.sum(weights * weighted_sum_candidates, dim=1, keepdim=True).to(flair.device)
 
         return weighting
 
@@ -122,16 +122,17 @@ class TextClassifier(flair.nn.Model):
         if not self.attention:
             self.document_embeddings.embed(sentences)
             text_embedding_list = [sentence.get_embedding().unsqueeze(0) for sentence in sentences]
-            text_embedding_tensor = torch.cat(text_embedding_list, 0)
+            text_embedding_tensor = torch.cat(text_embedding_list, 0).to(flair.device)
             label_scores = self.decoder(text_embedding_tensor)
 
         # experimental - use attention
         else:
+
             # define and register a hook that is called during the forward pass to get internal RNN states
             self.rnn_output = None
 
             def hook(m, i, o):
-                self.rnn_output = o.data
+                self.rnn_output = o
 
             handle = self.document_embeddings.dropout.register_forward_hook(hook)
 