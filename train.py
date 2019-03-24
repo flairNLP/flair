07efbac1275c0c59281121b73052db5fafe57cb4
Stefan Schweter <stefan@schweter.it>
2019-03-25 00:36:14 +0100

    train: use upos for training on universal dependencies

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ corpus: TaggedCorpus = NLPTaskDataFetcher.load_corpus(NLPTask.UD_ENGLISH)
 print(corpus)
 
 # 2. what tag do we want to predict?
-tag_type = 'pos'
+tag_type = 'upos'
 
 # 3. make the tag dictionary from the corpus
 tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
