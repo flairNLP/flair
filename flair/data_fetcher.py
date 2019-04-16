7e5b9dea15f3052b523de08d29dcc44b6b15e9d1
Alan Akbik <akbika@us.ibm.com>
2019-04-16 11:06:42 +0200

    Update data_fetcher.py

--- flair/data_fetcher.py
+++ flair/data_fetcher.py
@@ -213,7 +213,7 @@ class NLPTaskDataFetcher:
             NLPTask.AG_NEWS.value,
             NLPTask.TREC_6.value,
             NLPTask.TREC_50.value,
-            NLPTask.REGRESSION.value.
+            NLPTask.REGRESSION.value,
         ]:
             use_tokenizer: bool = False if task in [
                 NLPTask.TREC_6.value,
