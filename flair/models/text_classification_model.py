750ad75d7960dbfae87cfb17d6b8798641a8fe48
aakbik <alan.akbik@zalando.de>
2019-06-20 16:07:38 +0200

    remove focal loss from this PR

--- flair/models/text_classification_model.py
+++ flair/models/text_classification_model.py
@@ -1,3 +1,5 @@
+import math
+import warnings
 import logging
 from pathlib import Path
 from typing import List, Union
@@ -16,31 +18,8 @@ from flair.training_utils import (
     Metric,
     Result,
 )
-import torch.nn.functional as F
 
 log = logging.getLogger("flair")
-
-
-class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
-        super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        self.gamma = gamma
-        self.logits = logits
-        self.reduce = reduce
-
-    def forward(self, inputs, targets):
-        if self.logits:
-            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
-        else:
-            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
-        pt = torch.exp(-BCE_loss)
-        F_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
-
-        if self.reduce:
-            return torch.mean(F_loss)
-        else:
-            return F_loss
 
 
 class TextClassifier(flair.nn.Model):
@@ -81,7 +60,6 @@ class TextClassifier(flair.nn.Model):
             self.loss_function = nn.BCEWithLogitsLoss()
         else:
             self.loss_function = nn.CrossEntropyLoss()
-            # self.loss_function = FocalLoss()
 
         # auto-spawn on GPU if available
         self.to(flair.device)
