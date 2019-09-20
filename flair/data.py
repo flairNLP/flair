778ace95f614f07fcdc93d988cb2dd5463caeec9
eurekaqq_lin <eurekaqq_lin@cyberon.com.tw>
2019-09-20 11:56:22 +0800

    fixed coding style

--- flair/data.py
+++ flair/data.py
@@ -854,12 +854,12 @@ class Sentence(DataPoint):
     def _restore_windows_1252_characters(self, text:str)->str:
         def to_windows_1252(match):
             try:
-                return bytes([ord(match.group(0))]).decode('windows-1252')
+                return bytes([ord(match.group(0))]).decode("windows-1252")
             except UnicodeDecodeError:
-                # No character at the corresponding code point: remove it.
-                return ''
+                # No character at the corresponding code point: remove it
+                return ""
 
-        return re.sub(r'[\u0080-\u0099]', to_windows_1252, text)
+        return re.sub(r"[\u0080-\u0099]", to_windows_1252, text)
 
 
 class Image(DataPoint):
