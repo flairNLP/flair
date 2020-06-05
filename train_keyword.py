a5d179646660ea6822fc07b6bbcf08110076a602
whoisjones <jonas.golde@gmail.com>
2020-06-05 18:27:58 +0200

    #1647 changed some parameters

--- train_keyword.py
+++ train_keyword.py
@@ -15,13 +15,10 @@ from flair.training_utils import EvaluationMetric
 from flair.visual.training_curves import Plotter
 
 data = [
-        (flair.datasets.INSPEC(), 'keyphrase-tagger-inspec')
-        ]
-
-"""
+        (flair.datasets.INSPEC(), 'keyphrase-tagger-inspec'),
         (flair.datasets.SEMEVAL2010(), 'keyphrase-tagger-semeval2010'),
         (flair.datasets.SEMEVAL2017(), 'keyphrase-tagger-semeval2017')
-"""
+        ]
 
 for corpus_object, path in data:
     # 1. get the corpus
@@ -35,17 +32,15 @@ for corpus_object, path in data:
     tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
     print(tag_dictionary)
 
-    """
-            [
-            WordEmbeddings("crawl"),
-            FlairEmbeddings("news-forward"),
-            FlairEmbeddings("news-backward")
-        ],
-    """
 
     # initialize embeddings
     embedding_types = \
     [
+        [
+            WordEmbeddings("crawl"),
+            FlairEmbeddings("news-forward"),
+            FlairEmbeddings("news-backward")
+        ],
         [
             TransformerWordEmbeddings('/tmp/scibert_scivocab_uncased')
         ]
@@ -74,7 +69,7 @@ for corpus_object, path in data:
         trainer.train(
             "resources/taggers/{}".format(path),
             learning_rate=0.05,
-            mini_batch_size=8,
+            mini_batch_size=16,
             max_epochs=150,
             embeddings_storage_mode='gpu'
         )
