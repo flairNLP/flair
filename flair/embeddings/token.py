f1aacaba8a0d35c57e6877a4e6485599a864114a
mauryaland <amaury@fouret.org>
2020-07-17 18:44:58 +0200

    Adapt BytePairEmbeddings class to new version of bpemb
    Now, we can load custom BPEmb

--- flair/embeddings/token.py
+++ flair/embeddings/token.py
@@ -1548,18 +1548,38 @@ class BPEmbSerializable(BPEmb):
 class BytePairEmbeddings(TokenEmbeddings):
     def __init__(
         self,
-        language: str,
+        language: str = None,
         dim: int = 50,
         syllables: int = 100000,
         cache_dir=Path(flair.cache_root) / "embeddings",
+        model_file_path: Path = None,
+        embedding_file_path: Path = None,
+        **kwargs,
     ):
         """
         Initializes BP embeddings. Constructor downloads required files if not there.
         """
+        if language:
+            self.name: str = f"bpe-{language}-{syllables}-{dim}"
+        else:
+            assert (
+                model_file_path is not None and embedding_file_path is not None
+            ), "Need to specify model_file_path and embedding_file_path if no language is given in BytePairEmbeddings(...)"
+            dim=None
 
-        self.name: str = f"bpe-{language}-{syllables}-{dim}"
+        self.embedder = BPEmb(
+            lang=language,
+            vs=syllables,
+            dim=dim,
+            cache_dir=cache_dir,
+            model_file=model_file_path,
+            emb_file=embedding_file_path,
+            **kwargs,
+        )
+
+        if not language:
+            self.name: str = f"bpe-custom-{self.embedder.vs}-{self.embedder.dim}"
         self.static_embeddings = True
-        self.embedder = BPEmb(lang=language, vs=syllables, dim=dim, cache_dir=cache_dir)
 
         self.__embedding_length: int = self.embedder.emb.vector_size * 2
         super().__init__()
