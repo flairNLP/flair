ec68a65850a157708063c00c5e50d7f0f99664f2
alanakbik <alan.akbik@gmail.com>
2020-08-12 22:53:16 +0200

    GH-1753: fix error when deserializing GPU model on CPU

--- flair/embeddings/token.py
+++ flair/embeddings/token.py
@@ -1757,6 +1757,16 @@ class ELMoEmbeddings(TokenEmbeddings):
 
     def __setstate__(self, state):
         self.__dict__ = state
+
+        if re.fullmatch(r"cuda:[0-9]+", str(flair.device)):
+            cuda_device = int(str(flair.device).split(":")[-1])
+        elif str(flair.device) == "cpu":
+            cuda_device = -1
+        else:
+            cuda_device = 0
+
+        self.ee.cuda_device = cuda_device
+
         self.ee.elmo_bilm.to(device=flair.device)
         self.ee.elmo_bilm._elmo_lstm._states = tuple(
             [state.to(flair.device) for state in self.ee.elmo_bilm._elmo_lstm._states])
