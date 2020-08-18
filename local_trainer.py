1094a397fa703e1d900ad15fe7a5bb3123cf6505
whoisjones <jonas.golde@gmail.com>
2020-08-18 16:17:27 +0200

    added trainer file

---
+++ local_trainer.py
@@ -0,0 +1,31 @@
+import flair.datasets
+from flair.embeddings import POSEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+
+corpus = flair.datasets.WIKINER_ENGLISH()
+
+tag_type = 'ner'
+
+tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+print(tag_dictionary)
+
+embedding_types = [
+    POSEmbeddings(),
+]
+
+embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                        embeddings=embeddings,
+                                        tag_dictionary=tag_dictionary,
+                                        tag_type=tag_type,
+                                        use_crf=True)
+
+trainer: ModelTrainer = ModelTrainer(tagger, corpus)
+
+trainer.train('resources/taggers/ner-tagger-with-pos-embeddings',
+              learning_rate=0.1,
+              mini_batch_size=32,
+              max_epochs=150,
+              embeddings_storage_mode='gpu')
