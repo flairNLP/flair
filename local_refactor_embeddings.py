ca68be151f12c6cbcc741424527725922b517ff1
whoisjones <jonas.golde@gmail.com>
2020-08-26 22:18:10 +0200

    refactor normal embeddings

--- local_refactor_embeddings.py
+++ local_refactor_embeddings.py
@@ -4,18 +4,8 @@ import gensim
 
 for dim in [100, 400]:
 
-    embeddings = f"normal_embeddings_{dim}d"
-
-    cache_dir = Path("embeddings")
-
-    # part of speech embeddings
-    cached_path(f"glove_normal{dim}/vectors.txt", cache_dir=cache_dir)
-    embeddings = cached_path(
-        f"glove_normal{dim}/vectors.txt", cache_dir=cache_dir
-    )
-
     word_vectors = gensim.models.KeyedVectors.load_word2vec_format(
-        str(embeddings), binary=False
+        f"glove_normal{dim}/vectors.txt", binary=False
     )
 
     word_vectors.save(f"normal_embeddings_{dim}d")
