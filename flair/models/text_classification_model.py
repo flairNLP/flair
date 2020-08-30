311be11430ccc4785119d18fbb962629fe454bc2
alanakbik <alan.akbik@gmail.com>
2020-08-30 09:56:13 +0200

    GH-1831: move text classification models to HU server

--- flair/models/text_classification_model.py
+++ flair/models/text_classification_model.py
@@ -459,15 +459,10 @@ class TextClassifier(flair.nn.Model):
     def _fetch_model(model_name) -> str:
 
         model_map = {}
-        aws_resource_path = "https://s3.eu-central-1.amazonaws.com/alan-nlp/resources/models-v0.4"
         hu_path: str = "https://nlp.informatik.hu-berlin.de/resources/models"
 
         model_map["de-offensive-language"] = "/".join(
-            [
-                aws_resource_path,
-                "classy-offensive-de-rnn-cuda%3A0",
-                "germ-eval-2018-task-1-v0.4.pt",
-            ]
+            [hu_path, "de-offensive-language", "germ-eval-2018-task-1-v0.4.pt"]
         )
 
         # English sentiment models
