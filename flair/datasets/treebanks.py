09ee3d8f21c4fae740a46906d9ac2ba5fa482389
Nikolas Malamas <malamasn@hotmail.com>
2020-11-02 18:33:52 +0200

    Add Greek UD Treebank

--- flair/datasets/treebanks.py
+++ flair/datasets/treebanks.py
@@ -1097,3 +1097,30 @@ class UD_CHINESE_KYOTO(UniversalDependenciesCorpus):
         )
 
         super(UD_CHINESE_KYOTO, self).__init__(data_folder, in_memory=in_memory)
+
+
+class UD_GREEK(UniversalDependenciesCorpus):
+    def __init__(self, base_path: Union[str, Path] = None, in_memory: bool = True):
+
+        if type(base_path) == str:
+            base_path: Path = Path(base_path)
+
+        # this dataset name
+        dataset_name = self.__class__.__name__.lower()
+
+        # default dataset folder is the cache root
+        if not base_path:
+            base_path = Path(flair.cache_root) / "datasets"
+        data_folder = base_path / dataset_name
+
+        # download data if necessary
+        web_path = "https://raw.githubusercontent.com/UniversalDependencies/UD_Greek-GDT/master"
+        cached_path(f"{web_path}/el_gdt-ud-dev.conllu", Path("datasets") / dataset_name)
+        cached_path(
+            f"{web_path}/el_gdt-ud-test.conllu", Path("datasets") / dataset_name
+        )
+        cached_path(
+            f"{web_path}/el_gdt-ud-train.conllu", Path("datasets") / dataset_name
+        )
+
+        super(UD_GREEK, self).__init__(data_folder, in_memory=in_memory)
