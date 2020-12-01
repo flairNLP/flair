447c1fc7b27ccf802d0f0cca683916fa6c5c6467
Philipp Harnisch <p.harnisch.privat@gmail.com>
2020-12-01 14:13:56 +0100

    first methods working of TARSSequenceTagger, but mostly WIP

--- flair/models/__init__.py
+++ flair/models/__init__.py
@@ -1,3 +1,4 @@
 from .sequence_tagger_model import SequenceTagger, MultiTagger
+from .tars_sequence_tagger_model import TARSSequenceTagger
 from .language_model import LanguageModel
 from .text_classification_model import TextClassifier
