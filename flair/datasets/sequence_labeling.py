85e38e90ae43277efee83c21408a56da61753ab7
melvelet <richard@herrmann-mschwaben.de>
2020-12-08 18:23:39 +0100

    change connl04 to conll_04

--- flair/datasets/sequence_labeling.py
+++ flair/datasets/sequence_labeling.py
@@ -2454,7 +2454,7 @@ class TURKU_NER(ColumnCorpus):
             **corpusargs,
         )
 
-class CONLL04(ColumnCorpus):
+class CONLL_04(ColumnCorpus):
     def __init__(
             self,
             base_path: Union[str, Path] = None,
@@ -2464,7 +2464,7 @@ class CONLL04(ColumnCorpus):
             **corpusargs,
     ):
         """
-        Initialize the CoNLL04. The first time you call this constructor it will automatically
+        Initialize the CoNLL_04. The first time you call this constructor it will automatically
         download the dataset.
         :param base_path: Default is None, meaning that corpus gets auto-downloaded and loaded. You can override this
         to point to a different folder but typically this should not be necessary.
@@ -2496,7 +2496,7 @@ class CONLL04(ColumnCorpus):
         cached_path(f"{conll_path}/{test_file}", Path("datasets") / dataset_name)
         cached_path(f"{conll_path}/{train_file}", Path("datasets") / dataset_name)
 
-        super(CONLL04, self).__init__(
+        super(CONLL_04, self).__init__(
             data_folder,
             columns,
             dev_file=dev_file,
