a9a7a3efec6d945953f2c152a6bfb4c8ebdfb139
whoisjones <jonas.golde@gmail.com>
2020-12-22 13:15:03 +0100

    added custom instance lru cache which let the model be pickled but also uses cache for get_cached_vec.

--- flair/file_utils.py
+++ flair/file_utils.py
@@ -9,6 +9,7 @@ import logging
 import shutil
 import tempfile
 import re
+import functools
 from urllib.parse import urlparse
 
 import mmap
@@ -319,3 +320,14 @@ class Tqdm:
         new_kwargs = {"mininterval": Tqdm.default_mininterval, **kwargs}
 
         return _tqdm(*args, **new_kwargs)
+
+def instance_lru_cache(*cache_args, **cache_kwargs):
+    def decorator(func):
+        @functools.wraps(func)
+        def create_cache(self, *args, **kwargs):
+            instance_cache = functools.lru_cache(*cache_args, **cache_kwargs)(func)
+            instance_cache = instance_cache.__get__(self, self.__class__)
+            setattr(self, func.__name__, instance_cache)
+            return instance_cache(*args, **kwargs)
+        return create_cache
+    return decorator
