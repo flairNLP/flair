966339a76a5a04005950c0aad7a1b081ba071002
Stefan Schweter <stefan@schweter.it>
2020-12-24 14:32:43 +0100

    models: style improvements

--- flair/models/sequence_tagger_model.py
+++ flair/models/sequence_tagger_model.py
@@ -1138,8 +1138,8 @@ class SequenceTagger(flair.nn.Model):
 
             url = hf_hub_url(model_id=model_name, revision=revision, filename=hf_model_name)
             model_name = cached_download(url=url, library_name="flair",
-                                                       library_version=flair.__version__,
-                                                       cache_dir=flair.cache_root)
+                                         library_version=flair.__version__,
+                                         cache_dir=flair.cache_root)
 
         return model_name
 
