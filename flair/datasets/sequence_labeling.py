9a83494b797a293057d89665f8140625dbc637a0
Jabbawukis <44141348+Jabbawukis@users.noreply.github.com>
2021-01-02 23:05:10 +0100

    Update sequence_labeling.py

--- flair/datasets/sequence_labeling.py
+++ flair/datasets/sequence_labeling.py
@@ -589,7 +589,7 @@ class STACKOVERFLOW_NER(ColumnCorpus):
 
         """The Datasets are represented in the Conll format.
            In this format each line of the Dataset is in the following format:
-           <word>+"\ t"+<NE>"\ t"+<word>+"\ t"<markdown>
+           <word>+"\t"+<NE>"\t"+<word>+"\t"<markdown>
            The end of sentence is marked with an empty line.
            In each line NE represented the human annotated named entity 
            and <markdown> represented the code tags provided by the users who wrote the posts.
