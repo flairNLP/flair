1659e66b2ef2c473ca058e61215c4f9b36ae088a
Philipp Harnisch <p.harnisch.privat@gmail.com>
2021-01-28 12:25:44 +0100

    reintroduced tag_to_bio2 formatting in ColumnCorpus

--- flair/datasets/sequence_labeling.py
+++ flair/datasets/sequence_labeling.py
@@ -232,6 +232,10 @@ class ColumnDataset(FlairDataset):
             # if sentence ends, convert and return
             if self.__line_completes_sentence(line):
                 if len(sentence) > 0:
+                    if self.tag_to_bio2 is not None:
+                        sentence.convert_tag_scheme(
+                            tag_type=self.tag_to_bio2, target_scheme="iob"
+                        )
                     if self.tag_to_bioes is not None:
                         sentence.convert_tag_scheme(
                             tag_type=self.tag_to_bioes, target_scheme="iobes"
@@ -249,6 +253,10 @@ class ColumnDataset(FlairDataset):
         # check if this sentence is a document boundary
         if sentence.to_original_text() == self.document_separator_token: sentence.is_document_boundary = True
 
+        if self.tag_to_bio2 is not None:
+            sentence.convert_tag_scheme(
+                tag_type=self.tag_to_bio2, target_scheme="iob"
+            )
         if self.tag_to_bioes is not None:
             sentence.convert_tag_scheme(
                 tag_type=self.tag_to_bioes, target_scheme="iobes"
