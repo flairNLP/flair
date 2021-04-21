456473da9fccc66b32957c6dc5907cc00d264849
Leonard-Hangen <56963352+Leonard-Hangen@users.noreply.github.com>
2021-04-21 14:36:58 +0200

    Add Luganda NER Support
    Luganda or the Ganda language is a Bantu language spoken in the African Great Lakes region. It is one of the major languages in Uganda and is spoken by more than eight million Baganda and other people. ~Source Wikipedia: (https://en.wikipedia.org/wiki/Luganda)

--- flair/datasets/__init__.py
+++ flair/datasets/__init__.py
@@ -27,6 +27,7 @@ from .sequence_labeling import INSPEC
 from .sequence_labeling import JAPANESE_NER
 from .sequence_labeling import KINYARWANDA_NER
 from .sequence_labeling import LER_GERMAN
+from .sequence_labeling import LUGANDA_NER
 from .sequence_labeling import LUO_NER
 from .sequence_labeling import MIT_MOVIE_NER_SIMPLE
 from .sequence_labeling import MIT_MOVIE_NER_COMPLEX
