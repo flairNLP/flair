818ab520da7545a851176139cccba48a5d27afae
melvelet <richard@herrmann-mschwaben.de>
2021-04-23 13:23:47 +0200

    remove temporary tags, refactor function

--- flair/datasets/sequence_labeling.py
+++ flair/datasets/sequence_labeling.py
@@ -231,33 +231,43 @@ class ColumnDataset(FlairDataset):
 
             # if sentence ends, convert and return
             if self.__line_completes_sentence(line):
-                if len(sentence) > 0:
-                    if self.tag_to_bioes is not None:
-                        sentence.convert_tag_scheme(
-                            tag_type=self.tag_to_bioes, target_scheme="iobes"
-                        )
-                    sentence.relations = sentence.build_relations()
-                    # check if this sentence is a document boundary
-                    if sentence.to_original_text() == self.document_separator_token:
-                        sentence.is_document_boundary = True
-                    return sentence
+                # if len(sentence) > 0:
+                #     if self.tag_to_bioes is not None:
+                #         sentence.convert_tag_scheme(
+                #             tag_type=self.tag_to_bioes, target_scheme="iobes"
+                #         )
+                #
+                #     sentence.relations = sentence.build_relations()
+                #     for token in sentence:
+                #         token.remove_labels("relation")
+                #         token.remove_labels("relation_dep")
+                #
+                #     # check if this sentence is a document boundary
+                #     if sentence.to_original_text() == self.document_separator_token:
+                #         sentence.is_document_boundary = True
+                #     return sentence
+                break
 
             # otherwise, this line is a token. parse and add to sentence
-            else:
-                token = self._parse_token(line)
-                sentence.add_token(token)
+            # else:
+            token = self._parse_token(line)
+            sentence.add_token(token)
 
         # check if this sentence is a document boundary
         if sentence.to_original_text() == self.document_separator_token: sentence.is_document_boundary = True
 
         sentence.relations = sentence.build_relations()
+        for token in sentence:
+            token.remove_labels("relation")
+            token.remove_labels("relation_dep")
 
         if self.tag_to_bioes is not None:
             sentence.convert_tag_scheme(
                 tag_type=self.tag_to_bioes, target_scheme="iobes"
             )
 
-        if len(sentence) > 0: return sentence
+        if len(sentence) > 0:
+            return sentence
 
     def _parse_token(self, line: str) -> Token:
         fields: List[str] = re.split(self.column_delimiter, line.rstrip())
