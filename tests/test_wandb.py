2f28d837543b314d9115a573dec24621b328c9c7
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-05-03 19:53:53 +0530

    Update test

--- tests/test_wandb.py
+++ tests/test_wandb.py
@@ -1,51 +1,35 @@
 from flair.data import Corpus
-from flair.datasets import UD_ENGLISH
-from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings
-
-# 1. get the corpus
-corpus: Corpus = UD_ENGLISH().downsample(0.1)
-print(corpus)
-
-# 2. what tag do we want to predict?
-tag_type = 'pos'
-
-# 3. make the tag dictionary from the corpus
-tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
-print(tag_dictionary)
-
-# 4. initialize embeddings
-embedding_types = [
-
-    WordEmbeddings('glove'),
-
-    # comment in this line to use character embeddings
-    # CharacterEmbeddings(),
-
-    # comment in these lines to use flair embeddings
-    # FlairEmbeddings('news-forward'),
-    # FlairEmbeddings('news-backward'),
-]
-
-embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
-
-# 5. initialize sequence tagger
-from flair.models import SequenceTagger
-
-tagger: SequenceTagger = SequenceTagger(hidden_size=256,
-                                        embeddings=embeddings,
-                                        tag_dictionary=tag_dictionary,
-                                        tag_type=tag_type,
-                                        use_crf=True)
-
-# 6. initialize trainer
+from flair.datasets import TREC_6
+from flair.embeddings import WordEmbeddings, FlairEmbeddings, DocumentRNNEmbeddings
+from flair.models import TextClassifier
 from flair.trainers import ModelTrainer
 
-trainer: ModelTrainer = ModelTrainer(tagger, corpus, project_name='Sequence_tagger', run_name='example_pos')
 
-# 7. start training
-trainer.train('resources/taggers/example-pos',
+# 1. get the corpus
+corpus: Corpus = TREC_6()
+
+# 2. create the label dictionary
+label_dict = corpus.make_label_dictionary()
+
+# 3. make a list of word embeddings
+word_embeddings = [WordEmbeddings('glove')]
+
+# 4. initialize document embedding by passing list of word embeddings
+# Can choose between many RNN types (GRU by default, to change use rnn_type parameter)
+document_embeddings = DocumentRNNEmbeddings(word_embeddings, hidden_size=256)
+
+# 5. create the text classifier
+classifier = TextClassifier(document_embeddings, label_dictionary=label_dict)
+
+# 6. initialize the text classifier trainer
+trainer = ModelTrainer(classifier, corpus, project_name='Text_classifier', run_name='exp1')
+
+# 7. start the training
+trainer.train('resources/taggers/trec',
               learning_rate=0.1,
               mini_batch_size=32,
-              max_epochs=150, 
-              log_model_each_k_epochs=1
+              anneal_factor=0.5,
+              patience=5,
+              max_epochs=150,
+              log_model_each_k_epochs=1,
               )
