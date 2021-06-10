b1bfcb4a045f416f38aaf2bd0daf17d782a544ea
osanseviero <osanseviero@gmail.com>
2021-06-10 12:50:17 +0200

    remove test script

--- test_flair.py
+++
@@ -1,6 +0,0 @@
-from flair.models import SequenceTagger
-from flair.data import Sentence
-
-sentence = Sentence('I love Berlin .')
-tagger = SequenceTagger.load('flair/ner-english')
-print(tagger.push_to_hub('flair-ner-english3', commit_message="This is a new model"))
