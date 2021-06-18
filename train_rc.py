4b15eb0958833362a794bbcea5cdf0efd506188d
Alan Akbik <alan.akbik@gmail.com>
2021-06-18 15:55:19 +0200

    add script

--- train_rc.py
+++ train_rc.py
@@ -1,10 +1,8 @@
-from typing import List
+import torch.optim
 
 import flair.datasets
 from flair.data import Corpus
 from flair.embeddings import TransformerWordEmbeddings
-from flair.training_utils import EvaluationMetric
-from flair.visual.training_curves import Plotter
 
 # 1. get the corpus
 corpus: Corpus = flair.datasets.SEMEVAL_2010_TASK_8()
@@ -15,7 +13,7 @@ relation_label_dict = corpus.make_relation_label_dictionary(label_type="label")
 print(relation_label_dict.idx2item)
 
 # initialize embeddings
-embeddings = TransformerWordEmbeddings()
+embeddings = TransformerWordEmbeddings(layers="-1", fine_tune=True)
 
 # initialize sequence tagger
 from flair.models import RelationClassifier
@@ -32,17 +30,13 @@ model: RelationClassifier = RelationClassifier(
 from flair.trainers import ModelTrainer
 
 # initialize trainer
-trainer: ModelTrainer = ModelTrainer(model, corpus)
+trainer: ModelTrainer = ModelTrainer(model, corpus, optimizer=torch.optim.Adam)
 
 trainer.train(
     "resources/classifiers/example-rc",
-    learning_rate=0.1,
-    mini_batch_size=32,
+    learning_rate=3e-5,
+    mini_batch_size=4,
+    mini_batch_chunk_size=1,
     max_epochs=10,
-    # shuffle=False,
     shuffle=True,
 )
-
-plotter = Plotter()
-plotter.plot_training_curves("resources/taggers/example-ner/loss.tsv")
-plotter.plot_weights("resources/taggers/example-ner/weights.txt")
