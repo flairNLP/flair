c8a8b8911247f80084438400e8601f664280e62e
Alan Akbik <alan.akbik@gmail.com>
2021-08-20 14:43:21 +0200

    Dont use deep copy

--- flair/nn/model.py
+++ flair/nn/model.py
@@ -517,19 +517,19 @@ class DefaultClassifier(Classifier):
                     if self.multi_label:
                         sigmoided = torch.sigmoid(scores)  # size: (n_sentences, n_classes)
                         n_labels = sigmoided.size(1)
-                        for s_idx, (sentence, label) in enumerate(zip(sentences, label_candidates)):
+                        for s_idx, (sentence, label_candidate) in enumerate(zip(sentences, label_candidates)):
                             for l_idx in range(n_labels):
                                 label_value = self.label_dictionary.get_item_for_index(l_idx)
                                 if label_value == 'O': continue
                                 label_threshold = self._get_label_threshold(label_value)
                                 label_score = sigmoided[s_idx, l_idx].item()
                                 if label_score > label_threshold or return_probabilities_for_all_classes:
-                                    label.set_value(value=label_value, score=label_score)
-                                    sentence.add_complex_label(label_name, copy.deepcopy(label))
+                                    label = label_candidate.spawn(value=label_value, score=label_score)
+                                    sentence.add_complex_label(label_name, label)
                     else:
                         softmax = torch.nn.functional.softmax(scores, dim=-1)
                         n_labels = softmax.size(1)
-                        for s_idx, (sentence, label) in enumerate(zip(sentences, label_candidates)):
+                        for s_idx, (sentence, label_candidate) in enumerate(zip(sentences, label_candidates)):
                             idx = torch.argmax(softmax[s_idx])
                             for l_idx in range(n_labels):
                                 if not return_probabilities_for_all_classes and l_idx != idx:
@@ -538,8 +538,8 @@ class DefaultClassifier(Classifier):
                                     label_value = self.label_dictionary.get_item_for_index(l_idx)
                                     if label_value == 'O': continue
                                     label_score = softmax[s_idx, l_idx].item()
-                                    label.set_value(value=label_value, score=label_score)
-                                    sentence.add_complex_label(label_name, copy.deepcopy(label))
+                                    label = label_candidate.spawn(value=label_value, score=label_score)
+                                    sentence.add_complex_label(label_name, label)
 
                 store_embeddings(batch, storage_mode=embedding_storage_mode)
 
