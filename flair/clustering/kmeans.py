f4cafe113c07a829d567c3aa8f1969a7ad1ff921
jgolde <jonas.golde@gmail.com>
2022-01-11 13:21:23 +0100

    kmeans refactoring.

--- flair/clustering/kmeans.py
+++ flair/clustering/kmeans.py
@@ -1,7 +1,10 @@
 import logging
 
-from torch import tensor
-from flair.embeddings import DocumentEmbeddings
+import torch
+
+from flair.data import Corpus
+from flair.datasets import DataLoader
+from flair.embeddings import DocumentEmbeddings, SentenceTransformerDocumentEmbeddings
 
 from .base import ClusteringModel
 from .distance import get_cosine_distance
@@ -10,72 +13,44 @@ log = logging.getLogger("flair")
 
 
 class KMeans(ClusteringModel):
-    def __init__(self, k, embeddings: DocumentEmbeddings = None):
-        self.k = k
+
+    def __init__(self, n_clusters, embeddings: DocumentEmbeddings, corpus: Corpus, max_iter: int = 100):
+        self.n_clusters = n_clusters
+        self.centroids = dict
         self.embeddings = embeddings
-        self.k_points = []
-        self.predict = []
-        self.max_iteration = 100
+        self.corpus = corpus
+        self.max_iter = max_iter
 
-    def cluster(self, sentences: list, batch_size: int = 64) -> list:
-        log.debug("k Means cluster start with k: " + str(self.k))
+    def fit(self, batch_size: int = 64, p_norm: int = 2) -> list:
+        log.info("k-means clustering started with n_clusters: " + str(self.n_clusters))
 
-        for batch in DataLoader(sentences, batch_size=batch_size):
+        self._embed(batch_size=batch_size)
+
+        vectors = torch.stack([sentence.embedding for sentence in self.corpus.train])
+        self.centroids = self._init_centroids(vectors)
+
+        for iter in range(self.max_iter):
+            assignments = torch.argmin(torch.cdist(vectors, self.centroids, p=p_norm), dim=1)
+            self._adjust_centroids(vectors=vectors, assignments=assignments)
+            if iter > 0 and all(assignments == previous_assignments):
+                log.info("k-means converged. exiting...")
+                break
+            else:
+                previous_assignments = assignments
+
+        print()
+
+    def _embed(self, batch_size: int):
+        for batch in DataLoader(self.corpus.train, batch_size=batch_size):
             self.embeddings.embed(batch)
 
-        vectors = [sentence.embedding for sentence in sentences]
-        cluster_result = self.cluster_vectors(vectors)
+    def _init_centroids(self, vectors: torch.tensor):
+        return vectors[torch.randint(high=vectors.shape[0], size=(self.n_clusters,))]
 
-        for idx, sentence in enumerate(sentences):
-            sentence.set_label("cluster", str(self.predict[idx]))
-
-        return cluster_result
-
-    def cluster_vectors(self, vectors: list) -> list:
-        self.predict = [0] * len(vectors)
-
-        # init the k centroids
-        idxs = torch.from_numpy(np.random.choice(len(vectors), self.k))
-        self.k_points = [vectors[i] for i in idxs]
-
-        for i in range(self.max_iteration):
-            clusters = self.assign_clusters(vectors)
-            old_points = copy.copy(self.k_points)
-            self.adjust_centroids(clusters)
-
-            if self.is_algorithm_converged(old_points):
-                log.debug("Finished the k-Means with: " + str(i) + " iterations. ")
-                return clusters
-
-        log.debug("Finished the k-Means with maxItertation: " + str(self.max_iteration))
-        return clusters
 
     def is_algorithm_converged(self, old_points: list) -> bool:
         return bool(torch.all(torch.eq(torch.stack(self.k_points), torch.stack(old_points))))
 
-    def assign_clusters(self, vectors: list) -> list:
-        cluster = []
-        for i in range(self.k):
-            cluster.append([])
 
-        for idx, vector in enumerate(vectors):
-            cluster_index = self.assign(vector)
-
-            cluster[cluster_index].append(vector)
-            self.predict[idx] = cluster_index
-
-        return cluster
-
-    def assign(self, vector: tensor):
-        distance_to_centroid = get_cosine_distance(vector.repeat([self.k, 1]).t(), torch.stack(self.k_points).t())
-        index = torch.argmin(distance_to_centroid)
-
-        return int(index)
-
-    def adjust_centroids(self, clusters: list):
-        # TODO: ZeroDivisionError: division by zero
-        for idx, cluster in enumerate(clusters):
-            if len(cluster) != 0:
-                self.k_points[idx] = 1 / cluster.__len__() * sum(cluster)
-            else:
-                log.error("No element found in Cluster: ERROR")
+    def _adjust_centroids(self, vectors: torch.tensor, assignments: torch.tensor) -> torch.tensor:
+        self.centroids = torch.stack([torch.mean(vectors[assignments == cluster], dim=0) for cluster in range(self.n_clusters)])
