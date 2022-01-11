14095d9b97a72302e97fa889c325b874f2646bc5
jgolde <jonas.golde@gmail.com>
2022-01-11 14:15:26 +0100

    kmeans save and load functions

--- flair/clustering/kmeans.py
+++ flair/clustering/kmeans.py
@@ -1,20 +1,25 @@
+import os
+from pathlib import Path
+
 import logging
+import pickle
 
 import torch
 
 from flair.data import Corpus
 from flair.datasets import DataLoader
-from flair.embeddings import DocumentEmbeddings, SentenceTransformerDocumentEmbeddings
+from flair.embeddings import DocumentEmbeddings
+
+from tqdm import tqdm
 
 from .base import ClusteringModel
-from .distance import get_cosine_distance
 
 log = logging.getLogger("flair")
 
 
 class KMeans(ClusteringModel):
 
-    def __init__(self, n_clusters, embeddings: DocumentEmbeddings, corpus: Corpus, max_iter: int = 100):
+    def __init__(self, n_clusters: int, embeddings: DocumentEmbeddings, corpus: Corpus, max_iter: int = 100, centroids: torch.tensor = None):
         self.n_clusters = n_clusters
         self.centroids = dict
         self.embeddings = embeddings
@@ -33,24 +38,44 @@ class KMeans(ClusteringModel):
             assignments = torch.argmin(torch.cdist(vectors, self.centroids, p=p_norm), dim=1)
             self._adjust_centroids(vectors=vectors, assignments=assignments)
             if iter > 0 and all(assignments == previous_assignments):
-                log.info("k-means converged. exiting...")
+                log.info(f"k-means converged after {iter} iterations.")
                 break
             else:
                 previous_assignments = assignments
 
-        print()
+        self.save()
 
     def _embed(self, batch_size: int):
-        for batch in DataLoader(self.corpus.train, batch_size=batch_size):
+        log.info("Embed sentences...")
+        for batch in tqdm(DataLoader(self.corpus.train, batch_size=batch_size)):
             self.embeddings.embed(batch)
 
-    def _init_centroids(self, vectors: torch.tensor):
+    def _init_centroids(self, vectors: torch.tensor) -> torch.tensor:
         return vectors[torch.randint(high=vectors.shape[0], size=(self.n_clusters,))]
-
-
-    def is_algorithm_converged(self, old_points: list) -> bool:
-        return bool(torch.all(torch.eq(torch.stack(self.k_points), torch.stack(old_points))))
-
 
     def _adjust_centroids(self, vectors: torch.tensor, assignments: torch.tensor) -> torch.tensor:
         self.centroids = torch.stack([torch.mean(vectors[assignments == cluster], dim=0) for cluster in range(self.n_clusters)])
+
+    def save(self, path: str = "kmeans.pt"):
+        state_dict = {
+            "embeddings": self.embeddings.__class__,
+            "corpus": self.corpus.__class__,
+            "centroids": self.centroids,
+            "n_clusters": self.n_clusters
+        }
+
+        with open(path, 'wb') as f:
+            pickle.dump(state_dict, f)
+
+    @staticmethod
+    def load(path="kmeans.pt"):
+        with open(path, 'rb') as f:
+            state_dict = pickle.load(f)
+
+        model = KMeans(
+            n_clusters=state_dict["n_clusters"],
+            embeddings=state_dict["embeddings"](),
+            corpus=state_dict["corpus"](),
+            centroids=state_dict["centroids"]
+        )
+        return model
