c676702c27d93ce3917d06d21ace31f6387c138d
Pia Marlene Droop <pia.droop@gmail.com>
2022-03-09 23:02:20 +0100

    remove unused methods

--- flair/models/biomedical_entity_linking.py
+++ flair/models/biomedical_entity_linking.py
@@ -75,13 +75,11 @@ class TextPreprocess:
         lowercase=True,
         remove_punctuation=True,
         ignore_punctuations="",
-        typo_path=None,
     ):
         """
         :param typo_path str: path of known typo dictionary
         """
         self.lowercase = lowercase
-        self.typo_path = typo_path
         self.rmv_puncts = remove_punctuation
         self.punctuation = punctuation
         for ig_punc in ignore_punctuations:
@@ -90,22 +88,6 @@ class TextPreprocess:
             r"[\s{}]+".format(re.escape(self.punctuation))
         )
 
-        if typo_path:
-            self.typo2correction = self.load_typo2correction(typo_path)
-        else:
-            self.typo2correction = {}
-
-    def load_typo2correction(self, typo_path):
-        typo2correction = {}
-        with open(typo_path, mode="r", encoding="utf-8") as f:
-            lines = f.readlines()
-            for line in lines:
-                s = line.strip()
-                tokens = s.split("||")
-                value = "" if len(tokens) == 1 else tokens[1]
-                typo2correction[tokens[0]] = value
-
-        return typo2correction
 
     def remove_punctuation(self, phrase):
         phrase = self.rmv_puncts_regex.split(phrase)
@@ -113,24 +95,10 @@ class TextPreprocess:
 
         return phrase
 
-    def correct_spelling(self, phrase):
-        phrase_tokens = phrase.split()
-        phrase = ""
-
-        for phrase_token in phrase_tokens:
-            if phrase_token in self.typo2correction.keys():
-                phrase_token = self.typo2correction[phrase_token]
-            phrase += phrase_token + " "
-
-        phrase = phrase.strip()
-        return phrase
 
     def run(self, text):
         if self.lowercase:
             text = text.lower()
-
-        if self.typo_path:
-            text = self.correct_spelling(text)
 
         if self.rmv_puncts:
             text = self.remove_punctuation(text)
@@ -233,18 +201,6 @@ class BioSyn(object):
         assert self.sparse_weight is not None
 
         return self.sparse_weight
-
-    def save_model(self, path):
-        # save dense encoder
-        self.encoder.save_pretrained(path)
-        self.tokenizer.save_pretrained(path)
-        # save sparse encoder
-        sparse_encoder_path = os.path.join(path, "sparse_encoder.pk")
-        self.sparse_encoder.save_encoder(path=sparse_encoder_path)
-
-        sparse_weight_file = os.path.join(path, "sparse_weight.pt")
-        torch.save(self.sparse_weight, sparse_weight_file)
-        log.info("Sparse weight saved in {}".format(sparse_weight_file))
 
     def load_model(self, model_name_or_path):
         self.load_dense_encoder(model_name_or_path)
