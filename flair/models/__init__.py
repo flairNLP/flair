eaa4065c5b778ab6fb8567c068026b0296abcf21
dobbersc <29147025+dobbersc@users.noreply.github.com>
2022-04-11 22:52:53 +0200

    Expose `RelationClassifier`

--- flair/models/__init__.py
+++ flair/models/__init__.py
@@ -6,6 +6,7 @@ from .lemmatizer_model import Lemmatizer
 from .pairwise_classification_model import TextPairClassifier
 from .regexp_tagger import RegexpTagger
 from .relation_extractor_model import RelationExtractor
+from .relation_classifier_model import RelationClassifier
 from .sequence_tagger_model import MultiTagger, SequenceTagger
 from .tars_model import FewshotClassifier, TARSClassifier, TARSTagger
 from .text_classification_model import TextClassifier
@@ -18,6 +19,7 @@ __all__ = [
     "Lemmatizer",
     "TextPairClassifier",
     "RelationExtractor",
+    "RelationClassifier",
     "RegexpTagger",
     "MultiTagger",
     "SequenceTagger",
