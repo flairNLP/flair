5eed15d367613047bac022aa070dcaadffca90ea
thlor <thlor@users.noreply.github.com>
2022-10-13 16:49:31 +0200

    GH-2960 add docstring for anneal_against_dev_loss

--- flair/trainers/trainer.py
+++ flair/trainers/trainer.py
@@ -151,6 +151,8 @@ class ModelTrainer:
         :param save_final_model: If True, final model is saved
         :param anneal_with_restarts: If True, the last best model is restored when annealing the learning rate  # noqa: E501
         :param anneal_with_prestarts: If True, the model preceding the last best model is restored when annealing the learning rate  # noqa: E501
+        :param anneal_against_dev_loss: If True, the annealment is triggered when dev loss plateaus.  # noqa: E501
+         If False (default), it is triggered when dev score plateaus.
         :param shuffle: If True, data is shuffled during training
         :param param_selection_mode: If True, testing is performed against dev data. Use this mode when doing  # noqa: E501
         parameter selection.
