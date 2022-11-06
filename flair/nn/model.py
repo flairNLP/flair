a2bcf9c0e887a86574ddf7949080fc63e83f511e
Alan Akbik <alan.akbik@gmail.com>
2022-11-06 23:22:30 +0100

    Remove again

--- flair/nn/model.py
+++ flair/nn/model.py
@@ -590,7 +590,6 @@ class DefaultClassifier(Classifier[DT], typing.Generic[DT, DT2]):
         """Returns the data_points to which labels are added (Sentence, Span, Token, ... objects)"""
         raise NotImplementedError
 
-    @lru_cache(2048)
     def _get_data_points_for_batch(self, sentences: Tuple[DT]) -> List[DT2]:
         """Returns the data_points to which labels are added (Sentence, Span, Token, ... objects)"""
         return [
