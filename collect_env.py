bd1af11901a2b3689ff58c369f9d8011b3819e69
Benedikt Fuchs <benedikt.fuchs@rise-world.com>
2023-01-16 13:41:00 +0100

    fix collect_env hirachy and try formatting code

--- collect_env.py
+++ collect_env.py
@@ -5,11 +5,11 @@ import flair
 
 
 def main():
-    print("## Versions:")
-    print(f"### Flair\n{flair.__version__}")
-    print(f"### Pytorch\n{torch.__version__}")
-    print(f"### Transformers\n{transformers.__version__}")
-    print(f"## GPU\n{torch.cuda.is_available()}")
+    print("#### Versions:")
+    print(f"#### Flair\n{flair.__version__}")
+    print(f"#### Pytorch\n{torch.__version__}")
+    print(f"#### Transformers\n{transformers.__version__}")
+    print(f"#### GPU\n{torch.cuda.is_available()}")
 
 
 if __name__ == "__main__":
