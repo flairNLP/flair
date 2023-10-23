fe919382b10610521fa362229d03ebbf50ad74fc
Stefan Schweter <stefan@schweter.it>
2023-10-23 22:42:47 +0200

    datasets: add support for new German MobIE dataset

--- flair/datasets/sequence_labeling.py
+++ flair/datasets/sequence_labeling.py
@@ -4764,6 +4764,53 @@ class NER_NERMUD(MultiCorpus):
         )
 
 
+class NER_GERMAN_MOBIE(ColumnCorpus):
+    def __init__(
+        self,
+        base_path: Optional[Union[str, Path]] = None,
+        in_memory: bool = True,
+        **corpusargs,
+    ) -> None:
+        """Initialize the German MobIE NER dataset.
+
+        The German MobIE Dataset was introduced in the MobIE paper (https://aclanthology.org/2021.konvens-1.22/).
+
+        This is a German-language dataset that has been human-annotated with 20 coarse- and fine-grained entity types,
+        and it includes entity linking information for geographically linkable entities. The dataset comprises 3,232
+        social media texts and traffic reports, totaling 91K tokens, with 20.5K annotated entities, of which 13.1K are
+        linked to a knowledge base. In total, 20 different named entities are annotated.
+        :param base_path: Default is None, meaning that corpus gets auto-downloaded and loaded. You can override this
+        to point to a different folder but typically this should not be necessary.
+        :param in_memory: If True, keeps dataset in memory giving speedups in training. Not recommended due to heavy RAM usage.
+        """
+        base_path = flair.cache_root / "datasets" if not base_path else Path(base_path)
+        dataset_name = self.__class__.__name__.lower()
+        data_folder = base_path / dataset_name
+        data_path = flair.cache_root / "datasets" / dataset_name
+
+        columns = {0: "text", 3: "ner"}
+
+        train_data_file = data_path / "train.conll2003"
+        if not train_data_file.is_file():
+            temp_file = cached_path(
+                "https://github.com/DFKI-NLP/MobIE/raw/master/v1_20210811/ner_conll03_formatted.zip",
+                Path("datasets") / dataset_name,
+            )
+            from zipfile import ZipFile
+
+            with ZipFile(temp_file, "r") as zip_file:
+                zip_file.extractall(path=data_path)
+
+        super().__init__(
+            data_folder,
+            columns,
+            in_memory=in_memory,
+            comment_symbol=None,
+            document_separator_token="-DOCSTART-",
+            **corpusargs,
+        )
+
+
 class MASAKHA_POS(MultiCorpus):
     def __init__(
         self,
