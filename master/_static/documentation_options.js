fbd2a803901e9923ceb717b7b3437d1c32124bd9
alanakbik <alanakbik@users.noreply.github.com>
2023-12-13 04:24:31 +0000

    deploy: ce031f53116b89e6250c67fe0a43c377034a76ae

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.13.0',
+    VERSION: '0.13.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
