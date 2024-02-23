f3db229cf902aefb357b4f85d70903fca2c29fde
Mario Sänger <saengema@informatik.hu-berlin.de>
2024-02-23 11:21:23 +0100

    Fix model loading and update main documentation pages

--- flair/models/__init__.py
+++ flair/models/__init__.py
@@ -10,6 +10,7 @@ from .regexp_tagger import RegexpTagger
 from .relation_classifier_model import RelationClassifier
 from .relation_extractor_model import RelationExtractor
 from .sequence_tagger_model import SequenceTagger
+from .prefixed_tagger import PrefixedSequenceTagger
 from .tars_model import FewshotClassifier, TARSClassifier, TARSTagger
 from .text_classification_model import TextClassifier
 from .text_regression_model import TextRegressor
@@ -26,6 +27,7 @@ __all__ = [
     "RelationExtractor",
     "RegexpTagger",
     "SequenceTagger",
+    "PrefixedSequenceTagger",
     "TokenClassifier",
     "WordTagger",
     "FewshotClassifier",
