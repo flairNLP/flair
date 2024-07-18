f18a08929254f5def589c806866bb7e92f25b2ae
Alan Akbik <alan.akbik@gmail.com>
2024-07-18 16:31:57 +0200

    pin memory

--- flair/models/language_model.py
+++ flair/models/language_model.py
@@ -68,7 +68,7 @@ class SubwordTokenizer(LanguageModelTokenizer):
 
     def encode_as_sequence(self, texts: List[str]) -> torch.Tensor:
         lines = "".join(texts)
-        ids = self.subtokenizer(lines, return_tensors="pt").input_ids.flatten()
+        ids = self.subtokenizer(lines, return_tensors="pt").input_ids.flatten().pin_memory()
         return ids
 
     def vocab_size(self) -> int:
