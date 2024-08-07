b12a79fddc4fa22865231f034330a5c0546a8729
konstantin <konstantin_lukas@web.de>
2024-08-07 11:09:48 +0200

    changes

--- docs/conf.py
+++ docs/conf.py
@@ -82,6 +82,7 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_title = 'Flair Documentation'
 
 html_css_files = [
     'css/main.css',
