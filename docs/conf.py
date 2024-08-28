c26e59e231946aec5efac7731407f69e9a1ba6c3
konstantin <konstantin_lukas@web.de>
2024-08-28 12:55:12 +0200

    changes

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ def linkcode_resolve(*args):
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    'sphinx.ext.autosummary',
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
     "sphinx.ext.ifconfig",
