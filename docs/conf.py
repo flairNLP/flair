27f2f1a01a7a0dc11875243f55e672602171f278
konstantin <konstantin_lukas@web.de>
2024-08-28 12:59:30 +0200

    changes

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ extensions = [
     "sphinx_multiversion",
     "sphinx_design",
 ]
-
+autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
