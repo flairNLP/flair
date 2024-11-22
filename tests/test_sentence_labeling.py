c05614c75bcf2e14aa3439a3645b9bfaf7210b9d
Matt Buchovecky <mattb@ziprecruiter.com>
2024-11-22 11:53:14 -0800

    remove chunking logic to have simple sentence labeler. fix tests.

--- tests/test_sentence_labeling.py
+++ tests/test_sentence_labeling.py
@@ -1,9 +1,9 @@
-from typing import Dict, List
+from typing import cast
 
 import pytest
 
 from flair.data import Sentence
-from flair.training_utils import CharEntity, create_sentence_chunks
+from flair.training_utils import CharEntity, TokenEntity, create_labeled_sentence
 
 
 @pytest.fixture(params=["resume1.txt"])
@@ -28,7 +28,7 @@ def parsed_resume_dict(resume) -> dict:
 
 
 @pytest.fixture
-def small_token_limit_resume() -> Dict:
+def small_token_limit_resume() -> dict:
     return {
         "raw_text": "Professional Clown June 2020 - August 2021 Entertaining students of all ages. Blah Blah Blah "
         "Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah "
@@ -46,7 +46,7 @@ def small_token_limit_resume() -> Dict:
 
 
 @pytest.fixture
-def small_token_limit_response() -> List[Sentence]:
+def small_token_limit_response() -> list[Sentence]:
     """Recreates expected response Sentences."""
     chunk0 = Sentence("Professional Clown June 2020 - August 2021 Entertaining students of")
     chunk0[0:2].add_label("Professional Clown", "EXPERIENCE.TITLE")
@@ -63,28 +63,32 @@ def small_token_limit_response() -> List[Sentence]:
 
 class TestChunking:
     def test_empty_string(self):
-        sentences = create_sentence_chunks("", [])
+        sentences = create_labeled_sentence("", [])
         assert len(sentences) == 0
 
-    def check_split_entities(self, entity_labels, chunks, max_token_limit):
-        """Ensure that no entities are split over chunks (except entities longer than the token limit)."""
-        chunk_intervals = []
-        start_index = 0
-        for chunk in chunks:
-            end_index = start_index + len(chunk.text)
-            chunk_intervals.append((start_index, end_index))
-            start_index = end_index
+    def check_tokens(self, sentence: Sentence, expected_tokens: list[str]):
+        assert len(sentence.tokens) == len(expected_tokens)
+        assert [token.text for token in sentence.tokens] == expected_tokens
+        for token, expected_token in zip(sentence.tokens, expected_tokens):
+            assert token.text == expected_token
 
+    def check_token_entities(self, sentence: Sentence, expected_labels: list[TokenEntity]):
+        assert len(sentence.labels) == len(expected_labels)
+        for label, expected_label in zip(sentence.labels, expected_labels):
+
+            assert label.value == expected_label.label
+            span = cast(Sentence, label.data_point)
+            assert span.tokens[0]._internal_index is not None
+            assert span.tokens[0]._internal_index - 1 == expected_label.start_token_idx
+            assert span.tokens[-1]._internal_index is not None
+            assert span.tokens[-1]._internal_index - 1 == expected_label.end_token_idx
+
+    def check_split_entities(self, entity_labels, sentence: Sentence):
+        """Ensure that no entities are split over chunks (except entities longer than the token limit)."""
         for entity in entity_labels:
             entity_start, entity_end = entity.start_char_idx, entity.end_char_idx
-            entity_length = entity_end - entity_start
-
-            # Skip the check if the entity itself is longer than the maximum token limit
-            if entity_length > max_token_limit:
-                continue
-
-            assert any(
-                start <= entity_start and entity_end <= end for start, end in chunk_intervals
+            assert entity_start >= 0 and entity_end <= len(
+                sentence
             ), f"Entity {entity} is not within a single chunk interval"
 
     @pytest.mark.parametrize(
@@ -95,57 +99,71 @@ class TestChunking:
             ("this ", "this"),
         ],
     )
-    def test_short_text(self, test_text, expected_text):
+    def test_short_text(self, test_text: str, expected_text: str):
         """Short texts that should fit nicely into a single chunk."""
-        chunks = create_sentence_chunks(test_text, [])
-        assert chunks[0].text == expected_text
+        chunks = create_labeled_sentence(test_text, [])
+        assert chunks.text == expected_text
 
-    def test_create_flair_sentence(self, parsed_resume_dict):
-        chunks = create_sentence_chunks(parsed_resume_dict["raw_text"], parsed_resume_dict["entities"])
-        assert len(chunks) == 2
-
-        max_token_limit = 512  # default
-        assert all(len(c) <= max_token_limit for c in chunks)
-
-        self.check_split_entities(parsed_resume_dict["entities"], chunks, max_token_limit)
-
-    def test_small_token_limit(self, small_token_limit_resume, small_token_limit_response):
-        max_token_limit = 10  # test a small max token limit
-        chunks = create_sentence_chunks(
-            small_token_limit_resume["raw_text"], small_token_limit_resume["entities"], token_limit=max_token_limit
-        )
-
-        for response, expected in zip(chunks, small_token_limit_response):
-            assert response.to_tagged_string() == expected.to_tagged_string()
-
-        assert all(len(c) <= max_token_limit for c in chunks)
-
-        self.check_split_entities(small_token_limit_resume["entities"], chunks, max_token_limit)
+    def test_create_labeled_sentence(self, parsed_resume_dict: dict):
+        create_labeled_sentence(parsed_resume_dict["raw_text"], parsed_resume_dict["entities"])
 
     @pytest.mark.parametrize(
-        "test_text, entities, expected_chunks",
+        "test_text, entities, expected_tokens, expected_labels",
         [
             (
                 "Led a team of five engineers. It's important to note the project's success. We've implemented state-of-the-art technologies. Co-ordinated efforts with cross-functional teams.",
                 [
-                    CharEntity(0, 25, "RESPONSIBILITY", "Led a team of five engineers"),
-                    CharEntity(27, 72, "ACHIEVEMENT", "It's important to note the project's success"),
-                    CharEntity(74, 117, "ACHIEVEMENT", "We've implemented state-of-the-art technologies"),
-                    CharEntity(119, 168, "RESPONSIBILITY", "Co-ordinated efforts with cross-functional teams"),
+                    CharEntity(0, 28, "RESPONSIBILITY", "Led a team of five engineers"),
+                    CharEntity(30, 74, "ACHIEVEMENT", "It's important to note the project's success"),
+                    CharEntity(76, 123, "ACHIEVEMENT", "We've implemented state-of-the-art technologies"),
+                    CharEntity(125, 173, "RESPONSIBILITY", "Co-ordinated efforts with cross-functional teams"),
                 ],
                 [
-                    "Led a team of five engine er s. It 's important to note the project 's succe ss",
-                    ". We 've implemented state-of-the-art techno lo gies . Co-ordinated efforts with cross-functional teams .",
+                    "Led",
+                    "a",
+                    "team",
+                    "of",
+                    "five",
+                    "engineers",
+                    ".",
+                    "It",
+                    "'s",
+                    "important",
+                    "to",
+                    "note",
+                    "the",
+                    "project",
+                    "'s",
+                    "success",
+                    ".",
+                    "We",
+                    "'ve",
+                    "implemented",
+                    "state-of-the-art",
+                    "technologies",
+                    ".",
+                    "Co-ordinated",
+                    "efforts",
+                    "with",
+                    "cross-functional",
+                    "teams",
+                    ".",
+                ],
+                [
+                    TokenEntity(0, 5, "RESPONSIBILITY"),
+                    TokenEntity(7, 15, "ACHIEVEMENT"),
+                    TokenEntity(17, 21, "ACHIEVEMENT"),
+                    TokenEntity(23, 27, "RESPONSIBILITY"),
                 ],
             ),
         ],
     )
-    def test_contractions_and_hyphens(self, test_text, entities, expected_chunks):
-        max_token_limit = 20
-        chunks = create_sentence_chunks(test_text, entities, max_token_limit)
-        for i, chunk in enumerate(expected_chunks):
-            assert chunks[i].text == chunk
-        self.check_split_entities(entities, chunks, max_token_limit)
+    def test_contractions_and_hyphens(
+        self, test_text: str, entities: list[CharEntity], expected_tokens: list[str], expected_labels: list[TokenEntity]
+    ):
+        sentence = create_labeled_sentence(test_text, entities)
+        self.check_tokens(sentence, expected_tokens)
+        self.check_token_entities(sentence, expected_labels)
 
     @pytest.mark.parametrize(
         "test_text, entities",
@@ -156,36 +174,32 @@ class TestChunking:
             )
         ],
     )
-    def test_long_text(self, test_text, entities):
+    def test_long_text(self, test_text: str, entities: list[CharEntity]):
         """Test for handling long texts that should be split into multiple chunks."""
-        max_token_limit = 512
-        chunks = create_sentence_chunks(test_text, entities, max_token_limit)
-        assert len(chunks) > 1
-        assert all(len(c) <= max_token_limit for c in chunks)
-        self.check_split_entities(entities, chunks, max_token_limit)
+        create_labeled_sentence(test_text, entities)
 
     @pytest.mark.parametrize(
-        "test_text, entities, expected_chunks",
+        "test_text, entities, expected_labels",
         [
             (
                 "Hello! Is your company hiring? I am available for employment. Contact me at 5:00 p.m.",
                 [
                     CharEntity(0, 6, "LABEL", "Hello!"),
-                    CharEntity(7, 31, "LABEL", "Is your company hiring?"),
-                    CharEntity(32, 65, "LABEL", "I am available for employment."),
-                    CharEntity(66, 86, "LABEL", "Contact me at 5:00 p.m."),
+                    CharEntity(7, 30, "LABEL", "Is your company hiring?"),
+                    CharEntity(31, 61, "LABEL", "I am available for employment."),
+                    CharEntity(62, 85, "LABEL", "Contact me at 5:00 p.m."),
                 ],
                 [
-                    "Hello ! Is your company hiring ? I",
-                    "am available for employment . Con t",
-                    "act me at 5:00 p.m .",
+                    TokenEntity(0, 1, "LABEL"),
+                    TokenEntity(2, 6, "LABEL"),
+                    TokenEntity(7, 12, "LABEL"),
+                    TokenEntity(13, 18, "LABEL"),
                 ],
             )
         ],
     )
-    def test_text_with_punctuation(self, test_text, entities, expected_chunks):
-        max_token_limit = 10
-        chunks = create_sentence_chunks(test_text, entities, max_token_limit)
-        for i, chunk in enumerate(expected_chunks):
-            assert chunks[i].text == chunk
-        self.check_split_entities(entities, chunks, max_token_limit)
+    def test_text_with_punctuation(
+        self, test_text: str, entities: list[CharEntity], expected_labels: list[TokenEntity]
+    ):
+        sentence = create_labeled_sentence(test_text, entities)
+        self.check_token_entities(sentence, expected_labels)
