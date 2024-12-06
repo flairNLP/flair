fe7a7373c9efd20f6d8fb4721c82b08f0d4ffdf6
Benedikt Fuchs <e1526472@student.tuwien.ac.at>
2024-12-06 21:58:02 +0100

    bump version to 0.15.0

--- docs/conf.py
+++ docs/conf.py
@@ -6,8 +6,8 @@ import importlib_metadata
 # -- Project information -----------------------------------------------------
 from sphinx_github_style import get_linkcode_resolve
 
-version = "0.14.0"
-release = "0.14.0"
+version = "0.15.0"
+release = "0.15.0"
 project = "flair"
 author = importlib_metadata.metadata(project)["Author"]
 copyright = f"2023 {author}"
