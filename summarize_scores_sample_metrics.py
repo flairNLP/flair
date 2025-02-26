3f5d8a306fbf49503fec55fdc6505b4636012c80
Elena Merdjanovska <elenamerdzanovska@yahoo.com>
2025-02-26 09:49:49 +0100

    update test score summarization (formatting, enable merged f types)

--- summarize_scores_sample_metrics.py
+++ summarize_scores_sample_metrics.py
@@ -3,18 +3,19 @@ import numpy as np
 import pandas as pd
 
 categories_ids = ['4']
-f_types = ['f05', 'f1']#, 'f2']
+
 dataset='noise_crowd'
 
 for category_id in categories_ids:
     optimize_F1s_output = open('optimal_category'+category_id+'_test_scores.csv','w')
 
-    optimize_F1s_output.write('metric, f_score, modification, noise share, test score \n')
+    optimize_F1s_output.write('metric, f_score, modification, noise share, test score, std test score \n')
 
-    for f_type in f_types:
-        experiment_path = 'resources/relabel_cat'+category_id+'_ftype_'+f_type+os.sep+'category'+category_id
+    experiment_path = 'resources/relabel_cat'+category_id+os.sep+'category'+category_id
 
-        for metric in os.listdir(experiment_path):
+    for metric in os.listdir(experiment_path):
+
+        for f_type in os.listdir(os.path.join(experiment_path,metric)):
             print(os.path.join(experiment_path, metric, f_type))
             for modif in os.listdir(os.path.join(experiment_path, metric, f_type)):
                 results_path = os.path.join(experiment_path, metric, f_type, modif)
@@ -25,7 +26,7 @@ for category_id in categories_ids:
                     if os.path.isfile(fname):
                         with open(fname) as f:
                             lines = f.read().strip().split('\n')
-                            noise = lines[3].split(' ')[3]
+                            noise = lines[0].split(' ')[0]
                             noise_shares.append(float(noise))
                             print(noise_shares)
                 test_results_fname = results_path+os.sep+dataset+os.sep+'test_results.tsv'
@@ -33,6 +34,6 @@ for category_id in categories_ids:
                     results_df = pd.read_csv(test_results_fname, delimiter='\t', header=0)
                     print(results_df[['mean']].values)
                     score = float(results_df[['mean']].values[0])
-                    optimize_F1s_output.write(f"{''.join(metric.split('_')[1:])}, {f_type}, {modif}, {np.mean(noise_shares)}, {round(score, 4)}\n")
+                    stdev = float(results_df[['std']].values[0])
+                    optimize_F1s_output.write(f"{'_'.join(metric.split('_')[1:])}, {f_type}, {modif}, {np.mean(noise_shares):.3f}, {round(score, 4):.3f}, {round(stdev, 4):.3f}\n")
                     #optimize_F1s_output.write(f"{metric.split('_')[1:]}, {f_type}, {modif}, {0}, {0}\n")
-
