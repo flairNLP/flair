e6bec47b0b97dd6d4278b2f43b2fb90cf4109f72
Elena Merdjanovska <elenamerdzanovska@yahoo.com>
2025-03-04 07:55:50 +0100

    change summarization scripts to be functions; change paths

--- sample_metrics_scripts/merge_optimal_F_score_tables.py
+++ sample_metrics_scripts/merge_optimal_F_score_tables.py
@@ -1,20 +1,25 @@
 import pandas as pd
 import os
 
-base_path2 = '.'
+categories_ids = ['1','2','3','4']
 
-for category in ['1','2','3','4']:#, '2','3']:#,'3','4']:
-    data2 = pd.read_csv(base_path2+os.sep+'optimal_category'+category+'_test_scores.csv',header = 0, index_col=[0,1, 2])
+def merge_tables(results_tables_path):
+    base_path2 = results_tables_path
 
-    full_data = None
-    for mode in ['standard', 'EE']:
-        base_path1 = f'./noisebench_baselin/category0/{mode}_/noise_crowd'
+    for category in categories_ids:
 
-        data1 = pd.read_csv(base_path1+os.sep+'optimal_testF1s_merged_category'+category+'.csv',header = 0, index_col=[0,1])
-        print(data1)
+        data2 = pd.read_csv(base_path2+os.sep+'optimal_category'+category+'_test_scores.csv',header = 0, index_col=[0,1, 2])
 
-        if full_data is None:
-            full_data = pd.merge(data1, data2, left_index=True, right_index=True)
-        else:
-            full_data = pd.concat([full_data, pd.merge(data1, data2, left_index=True, right_index=True)], axis = 0)
-    full_data.to_csv(f'category{category}_merged_optimal_table.csv',index=True,header=True, float_format='%.3f')
+        full_data = None
+        for mode in ['standard', 'EE']:
+
+            base_path1 = f'{results_tables_path}/{mode}'
+
+            data1 = pd.read_csv(base_path1+os.sep+'optimal_F1s_merged_category'+category+'.csv',header = 0, index_col=[0,1])
+            print(data1)
+
+            if full_data is None:
+                full_data = pd.merge(data1, data2, left_index=True, right_index=True)
+            else:
+                full_data = pd.concat([full_data, pd.merge(data1, data2, left_index=True, right_index=True)], axis = 0)
+        full_data.to_csv(f'category{category}_merged_optimal_table.csv',index=True,header=True, float_format='%.3f')
