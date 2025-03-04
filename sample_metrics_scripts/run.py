14472cd84931b57c11f26f2d2ecc2379e2d7119f
Elena Merdjanovska <elenamerdzanovska@yahoo.com>
2025-03-04 14:37:03 +0100

    fix bugs in run.py

--- sample_metrics_scripts/run.py
+++ sample_metrics_scripts/run.py
@@ -9,31 +9,39 @@ import os
 
 def run(config):
 
-    seeds = [int(seed) for seed in config['parameters']['seeds']]
-    corpora = config['parameters']['corpora']
+    seeds = [int(seed) for seed in config['seeds']]
+    corpora = config['corpora']
 
     for corpus_name in corpora:
 
         for mode in config['parameters']['modes']:
+            temp_f1_scores = []
             for seed in seeds:
-                baseline_path = run_baseline(mode, seed,  corpus_name, config, int(config['parameters']['num_epochs']))
+                baseline_path, score = run_baseline(mode, seed,  corpus_name, config, int(config['parameters']['num_epochs']))
+
+                temp_f1_scores.append(score)
+
+            with open(baseline_path + os.sep + "test_results.tsv", "w", encoding='utf-8') as f:
+                f.write("params\tmean\tstd\n")
+                label = f"{str(config['parameters']['batch_size'])}_{str(config['parameters']['learning_rate'])}"
+                f.write(f"{label} \t{np.mean(temp_f1_scores)!s} \t {np.std(temp_f1_scores)!s} \n")
 
         optimize_F1s(config, corpus_name=corpus_name)
 
         for mode in config['parameters']['modes']:
-            for config_path in config['parameters']['configs_path'][mode]:
-                for dirpath,_,filenames in os.walk(config_path):
-                    for f in filenames:
-                        config_filepath = os.path.abspath(os.path.join(dirpath, f))
+            config_path = config['paths']['configs_path'][mode]
 
-                        with open(config_filepath) as json_file:
-                            config = json.load(json_file)
-                            
-                        main(config)
+            for dirpath,_,filenames in os.walk(config_path):
+                for f in filenames:
+                    config_filepath = os.path.abspath(os.path.join(dirpath, f))
 
-        summarize_test_scores(config['paths']['results_path'], corpus_name)
+                    with open(config_filepath) as json_file:
+                        config = json.load(json_file)
 
-        merge_tables(f"{config['paths']['results_path']}/{corpus_name}") 
+                    main(config)
+        summarize_test_scores(config['paths']['results_tables_path'], corpus_name)
+
+        merge_tables(f"{config['paths']['results_tables_path']}/{corpus_name}", config['parameters']['modes']) 
 
 
 if __name__ == "__main__":
