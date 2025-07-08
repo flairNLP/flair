7a24811c91d3a6fd8ca838a86d790b0f25340541
Teresa Löffelhardt <teresa.loeffelhardt@gmail.com>
2025-07-08 17:29:04 +0200

    set up consistency & realism experiment scripts

---
+++ _experiments_consistency.py
@@ -0,0 +1,9 @@
+import flair
+
+
+# for each dataset:
+    # for each noise model:
+        # for each seed:
+            # fine-tune model
+            # report (class-wise) F1, accuracy, precision, recall
+        # calculate stddev across F1
---
+++ _experiments_realism.py
@@ -0,0 +1,9 @@
+import flair
+
+
+# for each dataset
+    # for each noise model
+        # choose simulated data with median F1 score
+        # fine-tune 3 models
+        # report (class-wise) F1, accuracy, precision, recall
+        # calcuate mean F1 across fine-tuning seeds
