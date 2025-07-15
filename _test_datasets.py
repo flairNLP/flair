fd81c338cbf3377aa45683bf0cf5e28e346b18ff
Teresa Löffelhardt <teresa.loeffelhardt@gmail.com>
2025-07-15 12:05:19 +0200

    prep splits for IMDb, DBpedia, AGNews and AlleNoise (partly)

--- _test_datasets.py
+++ _test_datasets.py
@@ -1,20 +1,65 @@
 import csv
-import datasets
+# import datasets
+import _experiment_utils as exp_util
 import flair
+import json
+import pandas
 import pickle
 import sys
 
-# IMDb
+# IMDb clean
 # corpus_imdb = flair.datasets.IMDB(rebalance_corpus=False, noise=False)
+# exp_util.write_corpus_to_csv(simulation_corpus=corpus_imdb, data_folder="../data/IMDb", label_type="sentiment", splits='test')
+
+# imdb_path = "../data/IMDb"
+# with open(f"{imdb_path}/clean_test/full.csv", newline="", encoding="utf-8") as data_file:
+#     data = csv.reader(data_file, delimiter="\t")
+#     data_list = []
+#     for datum in data:
+#         data_list.append(datum)
+#     sorted_list = sorted(data_list, key=lambda pair: pair[0].lower())
+    
+#     with open(f"{imdb_path}/clean_test/train.csv", "w", newline="", encoding="utf-8") as train_file:
+#         new_data = csv.writer(train_file, delimiter="\t")
+#         new_data.writerows(sorted_list[:17500])    
+#     with open(f"{imdb_path}/clean_test/test.csv", "w", newline="", encoding="utf-8") as test_file:
+#         new_data = csv.writer(test_file, delimiter="\t")
+#         new_data.writerows(sorted_list[17500:21250])    
+#     with open(f"{imdb_path}/clean_test/dev.csv", "w", newline="", encoding="utf-8") as dev_file:
+#         new_data = csv.writer(dev_file, delimiter="\t")
+#         new_data.writerows(sorted_list[21250:25000])   
+
+ 
+# IMDb noisy
+# corpus_imdb = flair.datasets.IMDB(rebalance_corpus=False, noise=True)
+# exp_util.write_corpus_to_csv(simulation_corpus=corpus_imdb, data_folder="../data/IMDb/noisy_test", label_type="sentiment", splits='test')
+
+# imdb_path = "../data/IMDb"
+# with open(f"{imdb_path}/noisy_test/full_unsorted.csv", newline="", encoding="utf-8") as data_file:
+#     data = csv.reader(data_file, delimiter="\t")
+#     data_list = []
+#     for datum in data:
+#         data_list.append(datum)
+#     sorted_list = sorted(data_list, key=lambda pair: pair[0].lower())
+    
+#     with open(f"{imdb_path}/noisy_test/train.csv", "w", newline="", encoding="utf-8") as train_file:
+#         new_data = csv.writer(train_file, delimiter="\t")
+#         new_data.writerows(sorted_list[:17500])    
+#     with open(f"{imdb_path}/noisy_test/test.csv", "w", newline="", encoding="utf-8") as test_file:
+#         new_data = csv.writer(test_file, delimiter="\t")
+#         new_data.writerows(sorted_list[17500:21250])    
+#     with open(f"{imdb_path}/noisy_test/dev.csv", "w", newline="", encoding="utf-8") as dev_file:
+#         new_data = csv.writer(dev_file, delimiter="\t")
+#         new_data.writerows(sorted_list[21250:25000])   
 
 
-# # AG News
+# AG News
 # corpus_ag = flair.datasets.AGNEWS()
-# corpus_ag.make_label_dictionary(label_type='topic', min_count=0, add_unk=False, add_dev_test=True)
+# exp_util.write_corpus_to_csv(simulation_corpus=corpus_ag, data_folder="../data/AGNews/clean_suboriginal", label_type="topic", splits='test')
 
 
 # Noisy AG News
-# noisyagnews_path = "../../Data/NoisyAGNews"
+# noisyagnews_path = "../data/AGNews"
 
 # with open(f"{noisyagnews_path}/NoisyAGNews.pkl", "rb") as pickle_file:
 #     data = pickle.load(pickle_file)
@@ -32,14 +77,10 @@ import sys
 
 # with open(f"{noisyagnews_path}/train.csv", "w", newline="", encoding="utf-8") as train_file:
 #     new_data = csv.writer(train_file, delimiter="\t")
-#     new_data.writerows(data[:40000])
-# with open(f"{noisyagnews_path}/test.csv", "w", newline="", encoding="utf-8") as test_file:
-#     new_data = csv.writer(test_file, delimiter="\t")
-#     new_data.writerows(data[40000:45000])
+#     new_data.writerows(data[:42400])
 # with open(f"{noisyagnews_path}/dev.csv", "w", newline="", encoding="utf-8") as dev_file:
 #     new_data = csv.writer(dev_file, delimiter="\t")
-#     new_data.writerows(data[45000:])
-# #TODO: choose splits
+#     new_data.writerows(data[42400:])
 
 # corpus_noisyagnews_clean = flair.datasets.CSVClassificationCorpus(data_folder=noisyagnews_path,
 #                                                                 column_name_map={5: "text", 4: "label"},
@@ -91,56 +132,97 @@ import sys
 
 
 # AlleNoise
-allenoise_path = "../data/AlleNoise"
-category_mapping = {}
-with open(f"{allenoise_path}/category_mapping.csv", newline="", encoding="utf-8") as category_file:
-    categories = csv.reader(category_file, delimiter="\t")
-    next(categories)
-    for mapping in categories:
-        cat_id, cat_name = mapping
-        category_mapping[cat_id] = cat_name
+# allenoise_path = "../data/AlleNoise"
+# category_mapping = {}
+# with open(f"{allenoise_path}/category_mapping.csv", newline="", encoding="utf-8") as category_file:
+#     categories = csv.reader(category_file, delimiter="\t")
+#     next(categories)
+#     for mapping in categories:
+#         cat_id, cat_name = mapping
+#         category_mapping[cat_id] = cat_name
 
-with open(f"{allenoise_path}/full_dataset.csv", newline="", encoding="utf-8") as data_file:
-    data = csv.reader(data_file, delimiter="\t")
-    header = next(data)
-    data_list = []
-    for datum in data:
-        datum.append(category_mapping[datum[2]])
-        datum.append(category_mapping[datum[3]])
-        data_list.append(datum)
-    header.append("clean_category_label")
-    header.append("noisy_category_label")
+# with open(f"{allenoise_path}/full_dataset.csv", newline="", encoding="utf-8") as data_file:
+#     data = csv.reader(data_file, delimiter="\t")
+#     header = next(data)
+#     data_list = []
+#     for datum in data:
+#         datum.append(category_mapping[datum[2]])
+#         datum.append(category_mapping[datum[3]])
+#         data_list.append(datum)
+#     header.append("clean_category_label")
+#     header.append("noisy_category_label")
 
-    with open(f"{allenoise_path}/train.csv", "w", newline="", encoding="utf-8") as train_file:
-        new_data = csv.writer(train_file, delimiter="\t")
-        new_data.writerow(header)
-        new_data.writerows(data_list[:10000])    
-    with open(f"{allenoise_path}/test.csv", "w", newline="", encoding="utf-8") as test_file:
-        new_data = csv.writer(test_file, delimiter="\t")
-        new_data.writerow(header)
-        new_data.writerows(data_list[10000:11000])    
-    with open(f"{allenoise_path}/dev.csv", "w", newline="", encoding="utf-8") as dev_file:
-        new_data = csv.writer(dev_file, delimiter="\t")
-        new_data.writerow(header)
-        new_data.writerows(data_list[11000:12000])    
-#TODO: choose splits
+#     with open(f"{allenoise_path}/train_full.csv", "w", newline="", encoding="utf-8") as train_file:
+#         new_data = csv.writer(train_file, delimiter="\t")
+#         new_data.writerow(header)
+#         new_data.writerows(data_list)    
+    # with open(f"{allenoise_path}/test.csv", "w", newline="", encoding="utf-8") as test_file:
+    #     new_data = csv.writer(test_file, delimiter="\t")
+    #     new_data.writerow(header)
+    #     new_data.writerows(data_list[10000:11000])    
+    # with open(f"{allenoise_path}/dev.csv", "w", newline="", encoding="utf-8") as dev_file:
+    #     new_data = csv.writer(dev_file, delimiter="\t")
+    #     new_data.writerow(header)
+    #     new_data.writerows(data_list[11000:12000])    
+# #TODO: choose splits
 
-corpus_allenoise_clean = flair.datasets.CSVClassificationCorpus(data_folder=allenoise_path,
-                                                                column_name_map={1: "text", 4: "label"},
-                                                                label_type="category",
-                                                                name="allenoise_clean_corpus", 
-                                                                train_file="train.csv",
-                                                                test_file="test.csv",
-                                                                dev_file="dev.csv", 
-                                                                delimiter="\t",
-                                                                skip_header=True,                                            
-                                                                )
-label_dict = corpus_allenoise_clean.make_label_dictionary(label_type='category', min_count=0, add_unk=False, add_dev_test=True)
+# corpus_allenoise_clean = flair.datasets.CSVClassificationCorpus(data_folder=allenoise_path,
+#                                                                 column_name_map={1: "text", 4: "label"},
+#                                                                 label_type="category",
+#                                                                 name="allenoise_clean_corpus", 
+#                                                                 train_file="train_full.csv",
+#                                                                 test_file="test_empty.csv",
+#                                                                 dev_file="dev_empty.csv", 
+#                                                                 delimiter="\t",
+#                                                                 skip_header=True,                                            
+#                                                                 )
+# label_dict = corpus_allenoise_clean.make_label_dictionary(label_type='category', min_count=0, add_unk=False, add_dev_test=True)
 
-embeddings = flair.embeddings.TransformerDocumentEmbeddings('xlm-roberta-base', fine_tune=True)
-classifier = flair.models.TextClassifier(embeddings, label_dictionary=label_dict, label_type='category')
-trainer = flair.trainers.ModelTrainer(classifier, corpus_allenoise_clean)
-trainer.fine_tune('./_test_models/alle_noise', learning_rate=5.0e-5, mini_batch_size=4, max_epochs=1)
+# statistics = corpus_allenoise_clean.obtain_statistics(pretty_print=False)
+# sorted_label_support = sorted(statistics["TRAIN"]["number_of_documents_per_class"].items(), key=lambda x:x[1])
+# top_label_support = sorted_label_support[5442:]     # 250
+# top_labels = [label[0] for label in top_label_support]
+
+# support = 0
+# for label in top_label_support:
+#     if label[1] > 500:
+#         support += 500
+#     else:
+#         support += label[1]
+
+# (int) 0.85
+
+# with open(f"{allenoise_path}/full_dataset.csv", newline="", encoding="utf-8") as original_file:
+#     original_data = csv.reader(original_file, delimiter="\t")
+#     header = next(original_data)
+#     new_data = []
+#     label_support = {}
+#     for datum in original_data:
+#         if datum[4] in top_labels and datum[5] in top_labels:
+#             if datum[4] in label_support:
+#                 if label_support[datum[4]] < 500:
+#                     label_support[datum[4]] += 1
+#                     new_data.append(datum)
+#             else:
+#                 label_support[datum[4]] = 1
+#                 new_data.append(datum)
+
+#TODO: complete
+
+
+
+
+        
+
+
+
+# with open(f'{allenoise_path}/statistics_sorted_1000.json', 'w') as statistics_file:
+#     json.dump(sorted(corpus_allenoise_clean.obtain_statistics(pretty_print=False)["TRAIN"]["number_of_documents_per_class"].items(), key=lambda x:x[1])[4693:], statistics_file)
+
+# embeddings = flair.embeddings.TransformerDocumentEmbeddings('xlm-roberta-base', fine_tune=True)
+# classifier = flair.models.TextClassifier(embeddings, label_dictionary=label_dict, label_type='category')
+# trainer = flair.trainers.ModelTrainer(classifier, corpus_allenoise_clean)
+# trainer.fine_tune('./_test_models/alle_noise', learning_rate=5.0e-5, mini_batch_size=4, max_epochs=1)
 
 # corpus_allenoise_noisy = flair.datasets.CSVClassificationCorpus(data_folder=allenoise_path,
 #                                                                 column_name_map={1: "text", 5: "label"},
@@ -199,6 +281,29 @@ trainer.fine_tune('./_test_models/alle_noise', learning_rate=5.0e-5, mini_batch_
 #                                                                 skip_header=False,                                            
 #                                                                 )
 # corpus_dbpedia.make_label_dictionary(label_type='category', min_count=0, add_unk=False, add_dev_test=True)
+
+# with open(f"{dbpedia_path}/train_full.csv", newline="", encoding="utf-8") as train_full_file:
+#     data = csv.reader(train_full_file, delimiter="\t")
+#     data_list = []
+#     for datum in data:
+#         data_list.append(datum)
+    
+#     with open(f"{dbpedia_path}/train.csv", "w", newline="", encoding="utf-8") as train_file:
+#         new_data = csv.writer(train_file, delimiter="\t")
+#         new_data.writerows(data_list[:85000])    
+#     with open(f"{dbpedia_path}/dev.csv", "w", newline="", encoding="utf-8") as dev_file:
+#         new_data = csv.writer(dev_file, delimiter="\t")
+#         new_data.writerows(data_list[85000:95000])  
+
+# with open(f"{dbpedia_path}/test_full.csv", newline="", encoding="utf-8") as test_full_file:
+#     data = csv.reader(test_full_file, delimiter="\t")
+#     data_list = []
+#     for datum in data:
+#         data_list.append(datum)
+    
+#     with open(f"{dbpedia_path}/test.csv", "w", newline="", encoding="utf-8") as test_file:
+#         new_data = csv.writer(test_file, delimiter="\t")
+#         new_data.writerows(data_list[:10000])    
 
 
 # TREC Spam 2005
